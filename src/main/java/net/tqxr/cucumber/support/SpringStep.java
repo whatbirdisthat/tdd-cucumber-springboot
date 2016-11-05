7bce0ff6fbc765583b31c71d5af0550a05f68450
wbit <whatbirdisthat@gmail.com>
2016-11-06 03:10:13 +1100

    add a little fancy generic flavour to SpringStep to clean some code out of the stepdef sources

--- src/main/java/net/tqxr/cucumber/support/SpringStep.java
+++ src/main/java/net/tqxr/cucumber/support/SpringStep.java
@@ -18,6 +18,34 @@ import org.springframework.test.context.ContextConfiguration;
  * In short, best to use constructor injection, and avoid inheritance.
  */
 @ContextConfiguration(classes = {CucumberConfiguration.class})
-public abstract class SpringStep extends Assertions implements En {
+public abstract class SpringStep<T, TT> extends Assertions implements En {
+
+    protected DataContainer<T> dataContainer;
+    protected TT testComponent;
+
+    /**
+     * The empty constructor is called by cucumber, so we can put a hook in there
+     * to "automatically" call the method to set up all the test steps.
+     * <p>
+     * This means we can define the actual steps in methods of their own: making each step
+     * a *unit of work* - and we can *unit test* them!
+     */
+    protected SpringStep() {
+        setUpTestSteps();
+    }
+
+    /**
+     * Override this method to collect all the Given(), When(), Then() etc calls
+     * which makes the stepdef class a *lot* more testable.
+     * <p>
+     * If you put all your step definitions into the lambdas at constructor time,
+     * it's a world of pain trying to get a mock JavaBackend to fire the steps:
+     * it can be done, but we're not testing whether or not we can control cucumber.
+     * <p>
+     * We are verifying the intent of our own test steps, so we *remove* as much extraneous
+     * work as possible, °minimise* the number of moving parts and *focus* the testing effort
+     * on only the thing we are verifying.
+     */
+    protected abstract void setUpTestSteps();
 
 }
