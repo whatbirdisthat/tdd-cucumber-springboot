e3601e23b3831d84c8e7faa0d8c201461fcaa50a
wbit <whatbirdisthat@gmail.com>
2016-11-06 20:12:35 +1100

    add groovy script to show test report, so we can silence surefire entirely and still print a pretty agile-dox report

--- src/scripts/groovy/showTestReports.groovy
+++ src/scripts/groovy/showTestReports.groovy
@@ -1,2 +1,57 @@
-println 'HELLO WORLD'
+import groovy.io.FileType
+import net.tqxr.testframework.categories.TestDescription
+import net.tqxr.testframework.reporting.TestNameTransformer
 
+import static net.tqxr.testframework.reporting.AsciiColourHelper.*
+
+def testNameTransformer = new TestNameTransformer()
+
+final String CUTE_GRAPE = '\uD83C\uDF47'
+
+println "\n" + GREEN + CUTE_GRAPE + '  TEST RESULTS ' + CUTE_GRAPE + RESET + ""
+
+def list = []
+
+def dir = new File("target/surefire-reports/")
+dir.eachFileRecurse(FileType.FILES) { file ->
+    if (file.isFile() && file.absoluteFile.name.endsWith(".xml")) {
+        list << file
+    }
+}
+
+list.each {
+
+    def testsuite = new XmlParser().parseText(it.text)
+
+    def clz = Class.forName(testsuite.@name).getDeclaredAnnotation(TestDescription)
+    String val = clz ? clz.value() : ""
+    println val
+
+    if (testsuite.@failures != "0") {
+        print RED.underline()
+    } else {
+        print BLUE.underline()
+    }
+
+    print '' + testsuite.@name
+    println RESET
+
+    RED.reset()
+    BLUE.reset()
+
+    testsuite.testcase.each {
+
+        boolean isSkipped = it.skipped || false
+        boolean isFailed = it.failure || false
+
+        print "${testNameTransformer.passFailSkip(isFailed, isSkipped)}"
+        if (isSkipped) {
+            print BLACK.bright()
+        }
+        println "  " + testNameTransformer.transformTestName(it.@name as String) + RESET
+
+    }
+
+}
+
+println "\n\n"
