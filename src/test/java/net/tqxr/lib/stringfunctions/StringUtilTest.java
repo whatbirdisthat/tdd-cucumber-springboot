49e3492be4e877f787ae6b20f12393fc505597e3
wbit <whatbirdisthat@gmail.com>
2016-11-06 16:40:44 +1100

    added an ignored test to string util test, and put the whole class into ExampleTests category

--- src/test/java/net/tqxr/lib/stringfunctions/StringUtilTest.java
+++ src/test/java/net/tqxr/lib/stringfunctions/StringUtilTest.java
@@ -1,12 +1,14 @@
 package net.tqxr.lib.stringfunctions;
 
 import net.tqxr.testframework.categories.DeliberatelyFail;
+import net.tqxr.testframework.categories.ExampleTests;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@Category(ExampleTests.class)
 public class StringUtilTest {
 
     private StringUtil stringUtil;
@@ -53,6 +55,20 @@ public class StringUtilTest {
         assertThat(actual)
                 .isEqualTo(expected)
                 .withFailMessage("The string should be lower case now");
+
+    }
+
+    @Test
+    @Ignore("Test ignored.")
+    public void transformUpperIGNORE() throws Exception {
+
+        String expected = "STRINGZ";
+
+        String actual = stringUtil.transformUpper("string");
+
+        assertThat(actual)
+                .isEqualTo(expected)
+                .withFailMessage("The string should be upper case now");
 
     }
 
