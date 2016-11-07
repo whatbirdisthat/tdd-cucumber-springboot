afb433449bb071848e3914c365af0d71e0408e79
David Walters <David.Walters@tabcorp.com.au>
2016-11-07 19:27:18 +1100

    added jackson dependency to pom

---
+++ src/main/java/net/tqxr/cucumber/steps/redbook/comparison/meetings/MeetingsComparison.java
@@ -0,0 +1,7 @@
+package net.tqxr.cucumber.steps.redbook.comparison.meetings;
+
+/**
+ * Created by waltersd on 7/11/2016.
+ */
+public class MeetingsComparison {
+}
---
+++ src/main/java/net/tqxr/lib/redbook/Meeting.java
@@ -0,0 +1,7 @@
+package net.tqxr.lib.redbook;
+
+/**
+ * Created by waltersd on 7/11/2016.
+ */
+public class Meeting {
+}
